ce935f6806fa49af433d3dc0642ed702b149a24c
Lucas-Rapahel Müller <gitdev@LRM.photo>
2018-03-07 21:14:05 +0100

    Add test function

--- cnn/singlecharacter.py
+++ cnn/singlecharacter.py
@@ -8,6 +8,7 @@ import torch.optim as optim
 
 from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets, transforms, utils
 from torch.autograd import Variable
 
@@ -20,8 +21,10 @@ class Args:
         self.epochs = 10
         self.lr = 0.001
         self.momentum = 0.5
+        self.test_size = 0.1
         self.seed = np.random.randint(32000)
-        self.log_interval = 1
+        self.log_interval = 100
+        self.shuffle = True
 
 
 class Chardata(Dataset):
@@ -60,28 +63,69 @@ def train(epoch, data_iterator, criterion, optimizer):
                 100. * batch_idx / len(data_iterator), loss.data[0]))
 
 
+def test(data_iterator, criterion):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    accu = lambda x: x / len(data_iterator.dataset)
+    for data, target in data_iterator:
+        data, target = Variable(data.view(data.shape[0], 1, 32, 32)), Variable(target)
+        output = model(data)
+        test_loss += criterion(output, target).data[0]  # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.sampler)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(data_iterator.sampler),
+        100. * correct / len(data_iterator.sampler)))
+    return test_loss, accu(correct)
+
+
 if __name__ == '__main__':
     args = Args()
     torch.manual_seed(args.seed)
 
     # load the dataset
-    data = np.load('data.npy')
-
-    target = np.load('target.npy')
+    data = np.load('../data.npy')
+    target = np.load('../target.npy')
 
     data_character = Chardata(data=data, target=target)
-    data_loader = DataLoader(data_character,
+
+    num_samples = len(data_character)
+    indices = list(range(num_samples))
+    split = int(np.floor(args.test_size * num_samples))
+
+    if args.shuffle:
+        random_seed = 5
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_idx, test_idx = indices[split:], indices[:split]
+
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+
+    train_loader = DataLoader(data_character,
+                              batch_size=args.batch_size,
+                              sampler=train_sampler,
+                              # shuffle=True,
+                              drop_last=True)
+
+    test_loader = DataLoader(data_character,
                              batch_size=args.batch_size,
-                             shuffle=True,
-                             drop_last=True)
+                             sampler=test_sampler,
+                             # shuffle=True,
+                             drop_last=True
+                             )
 
     img_dim = 32 * 32
 
     model = ConvolutionalNN()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.NLLLoss()
 
     for epoch in range(args.epochs):
-        train(epoch, data_loader, criterion, optimizer)
+        train(epoch, train_loader, criterion, optimizer)
+        test(test_loader, criterion)
     # Save results.
     # torch.save(gen.state_dict(), 'torch_save/gen_state_dict.pth')
